759814aaf2f3781e3ed92b78278a1abbe8077100
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-03-24 01:39:33 +0100

    task 2

--- task2.py
+++ task2.py
@@ -12,3 +12,29 @@
 *
 ===================================================
 """
+
+string=str(input("Unesite string: "))
+
+velika_slova=string.upper()
+
+abeceda="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+ponavljanje=0
+
+for i in range(len(abeceda)):
+    br=0
+    for j in range(len(velika_slova)):
+        if abeceda[i]==velika_slova[j]:
+            br+=1
+    if br>ponavljanje:
+        naj_pon=abeceda[i]
+        ponavljanje=br
+
+def main():
+
+    najvise_ponavljanja = naj_pon
+    print("Najvise ponavljanja ima slovo: ", najvise_ponavljanja)
+
+main()
+
+
