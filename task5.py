4f8781691349f71266614b09f0a448999bf32dcb
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-03-24 01:40:26 +0100

    task 5

--- task5.py
+++ task5.py
@@ -13,3 +13,21 @@
 * Use main() function to test your solution.
 ===================================================
 """
+
+def averageval(lista):
+
+    zbir = 0
+
+    for i in lista:
+
+        zbir = zbir + i
+    prosjek = zbir / len(lista)
+
+    return prosjek
+
+def main():
+
+    prosjecna_vrijednost = averageval([4,6,8,10,12,14,16,18])
+    print(prosjecna_vrijednost)
+
+main()
